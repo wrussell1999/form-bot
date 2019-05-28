a603f514448b8d3c180fbbbda889269338c62462
Will Russell <wxr867@student.bham.ac.uk>
2019-05-28 21:35:17 +0100

    Basic bot working

--- formbot/formbot.py
+++ formbot/formbot.py
@@ -2,21 +2,27 @@ import discord
 from discord.ext import commands
 import json
 
-with open("config/config.json") as file:
+with open("config.json") as file:
     config = json.load(file)
 
-
+bot = commands.Bot(command_prefix='!')
 
 def main():
     print("Hello world!")
 
-
-def on_ready():
+@bot.event
+async def on_ready():
     print("Ready")
 
+@bot.event
 async def on_message(message):
+    print("hello")
     await message.channel.send("Hello")
 
-token = config['token']
+@bot.command
+async def mentor(ctx):
+    ctx.author.send("Hello, how can I help?")
+    ctx.message.delete()
 
+token = config['token']
 bot.run(token)
