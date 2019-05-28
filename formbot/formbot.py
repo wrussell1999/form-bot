3be4d5bccd8f8774bff2a325185f98dab5da1f0a
Will Russell <wxr867@student.bham.ac.uk>
2019-05-28 21:24:11 +0100

    Discord initial commit

--- formbot/formbot.py
+++ formbot/formbot.py
@@ -1,2 +1,22 @@
+import discord
+from discord.ext import commands
+import json
+
+with open("config/config.json") as file:
+    config = json.load(file)
+
+
+
 def main():
     print("Hello world!")
+
+
+def on_ready():
+    print("Ready")
+
+async def on_message(message):
+    await message.channel.send("Hello")
+
+token = config['token']
+
+bot.run(token)
